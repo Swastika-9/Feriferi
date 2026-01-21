e419c8b7d76fa34fa49eba6c2b6215ebde7ac97f
hridesh <hrideshrai0001@gmail.com>
2026-01-22 01:40:47 +0545

    ViewModel Factory for Ai bot

--- app/src/main/java/com/example/feriferi/viewmodel/ChatFairyViewModelFactory.kt
+++ app/src/main/java/com/example/feriferi/viewmodel/ChatFairyViewModelFactory.kt
@@ -1,4 +1,15 @@
 package com.example.feriferi.viewmodel
 
-class ChatFairyViewModelFactory {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.feriferi.repository.ChatFairyRepo
+
+class ChatFairyViewModelFactory(private val repo: ChatFairyRepo) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(ChatFairyViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return ChatFairyViewModel(repo) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
 }
