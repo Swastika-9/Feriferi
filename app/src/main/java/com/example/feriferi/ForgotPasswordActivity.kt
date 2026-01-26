687434bbd1f8ae14ebe2bd9d811974da7ca28a2f
Swastika-9 <swasty.009@gmail.com>
2026-01-27 00:16:37 +0545

    Forgot Password Complete

--- app/src/main/java/com/example/feriferi/ForgotPasswordActivity.kt
+++ app/src/main/java/com/example/feriferi/ForgotPasswordActivity.kt
@@ -1,6 +1,7 @@
 package com.example.feriferi
 
 import android.content.Context
+import com.example.feriferi.view.LoginActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
